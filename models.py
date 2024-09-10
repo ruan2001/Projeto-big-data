0d9361b435b72373b9e2dfe61e802704da3c501d
cesar.sena@maps.com.br <cesar.sena@maps.com.br>
2024-09-09 23:39:46 -0300

    Resolve conflits

--- models.py
+++ models.py
@@ -10,6 +10,9 @@ class Donor(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     contact = db.Column(db.String(50), nullable=False)
+    quantidade = db.Column(db.Integer, nullable=False)
+    idade_pet = db.Column(db.Integer, nullable=False)
+    idade = db.Column(db.Integer, nullable=False)
     donation_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
 
 
@@ -22,6 +25,15 @@ class FosterHome(db.Model):
 
 
 class Cat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    breed = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(50), nullable=False)  # "available", "adopted"
+    foster_home_id = db.Column(db.Integer, db.ForeignKey('foster_home.id'), nullable=True)
+
+
+class Dog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
